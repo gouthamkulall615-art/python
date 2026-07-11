c33a0f9360db51a85b645c3a46edb8cc766bc23f
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-11 09:18:30 +0530

    done with the variable,dataypes and rules to define the variable

---
+++ chapter2/02_datatypes.py
@@ -0,0 +1,11 @@
+a=1 # a is an integer
+
+b=5.22 #b is a floating point number
+
+c="goutham" #c is string 
+
+d=False # d is a boolean variable
+
+e=None # e is a none type variable
+
+
---
+++ chapter2/03_rules_variables.py
@@ -0,0 +1,21 @@
+a=23
+
+aaa=23
+
+
+harry=45
+
+_goutham=90
+
+
+
+# @goutham=56
+
+
+"""
+· A variable name can contain alphabets, digits, and underscores.
+. A variable name can only start with an alphabet and underscores.
+· A variable name can't start with a digit.
+. No while space is allowed to be used inside a variable name.
+
+"""
