e1d3f7a98b1a77b64d42bf4e286ba12a1a2d765f
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-13 18:17:49 +0530

    understood the list and there methods

---
+++ chapter4/01_list.py
@@ -0,0 +1,9 @@
+lists=["Apple","orange",5,90.08,False,"Goutham"]
+
+print(lists[0])
+
+lists[0]="Grapes"
+# list are mutable which means we can change
+
+print(lists[0])
+print(lists[1:4])
---
+++ chapter4/02_listMethods.py
@@ -0,0 +1,13 @@
+numbers=[1,9,3,5,6]
+# numbers.sort()
+# numbers.append(56)
+# numbers.reverse()
+# numbers.insert(3,8)
+
+# numbers.pop(3)
+
+# numbers.remove(6)
+
+
+
+print(numbers)
