f66be0a55e1731deb915fb24ffdd85ad2787dc75
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-15 18:57:52 +0530

    done with the chapter 7 :loops in the py

---
+++ chapter7/02_whileLoops.py
@@ -0,0 +1,12 @@
+# i=1
+# while(i<6):
+#     print(i)
+#     i+=1
+
+
+lists=[1,"harry",False,"34.6"]
+
+i=0;
+while(i<len(lists)):
+    print(lists[i])
+    i+=1
---
+++ chapter7/04_loopIterate.py
@@ -0,0 +1,11 @@
+# l=[1,4,7,5,9]
+# for i in l:
+#     print(i)
+
+# t=(6,4,45,33)
+# for i in t:
+#     print(i)
+
+s="Goutham"
+for i in s:
+    print(i)
