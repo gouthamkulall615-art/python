733429f00d348fa8735ba5e26b7fbd3caef0bbec
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-21 13:17:09 +0530

    done with the chapter 11 inheritance and property decorator and operator overloading

--- chapter11/06_propertyDecorators.py
+++ chapter11/06_propertyDecorators.py
@@ -7,11 +7,17 @@ class Employee:
 
     @property
     def name(self):
-        return self.ename
+        return f"{self.fname} {self.lname}"
+
+    @name.setter
+    def name(self, value):
+        self.fname = value.split(" ")[0]
+        self.lname = value.split(" ")[1]
+
 
 e = Employee()
 
 e.a = 45
-e.name="Goutham"
-print(e.name)
+e.name = "Goutham kulal"
+print(e.fname, e.lname)
 e.show()
