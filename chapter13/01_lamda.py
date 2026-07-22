a68adf96f897546b39173eeacb9fb88078cf972d
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-22 11:39:42 +0530

    done with the chapter 13

---
+++ chapter13/01_lamda.py
@@ -0,0 +1,12 @@
+# def square(n):
+#     return n*n
+
+
+# using the lamda funtion
+
+# square = lambda x: x * x
+
+sum = lambda a, b, c: a + b + c
+
+
+print(sum(11,2,3))
---
+++ chapter13/03_map_filter_reduce.py
@@ -0,0 +1,30 @@
+from functools import reduce
+
+# map example
+l = [1, 2, 3, 4, 5]
+
+# square = lambda x: x * x
+# sqList = map(square, l)
+# print(list(sqList))
+
+
+# filter Example
+# def even(n):
+#     if n % 2 == 0:
+#         return True
+#     return False
+
+
+# onlyEven = filter(even, l)
+
+# print(list(onlyEven))
+
+
+# Reduce Example
+
+
+def sum(a, b):
+    return a + b
+
+
+print(reduce(sum, l))
