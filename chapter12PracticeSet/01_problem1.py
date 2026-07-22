b82074e1b64f995c6acd6b933a96873700ed65d7
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-22 11:18:35 +0530

    done with the chapter 12 practice set

---
+++ chapter12PracticeSet/01_problem1.py
@@ -0,0 +1,25 @@
+try:
+
+    with open("chapter12PracticeSet/1.txt", "r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+try:
+
+    with open("chapter12PracticeSet/2.txt", "r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+try:
+
+    with open("chapter12PracticeSet/3.txt", "r") as f:
+        print(f.read())
+
+
+except Exception as e:
+    print(e)
+
+
+print("Thank you")
---
+++ chapter12PracticeSet/05_problem5.py
@@ -0,0 +1,5 @@
+n = 5
+
+table = [n * i for i in range(1, 11)]
+with open("tables.txt", "a") as f:
+    f.write(f"Table of {n} {str(table)}\n")
